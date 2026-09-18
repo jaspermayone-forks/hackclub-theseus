793acb1082bf9786615a7b2af7940ae0fcbab7ae
24c02 <163450896+24c02@users.noreply.github.com>
2026-09-18 18:45:32 -0400

    remove giant unsized SVG from printer setup banner

--- app/frontend/js/components/instant_print_window.js
+++ app/frontend/js/components/instant_print_window.js
@@ -8,9 +8,6 @@ export function InstantPrintWindow() {
             use(qz_settings.printer, (p) => (qz_settings.printer === 'pick a printer...' || !qz_settings.printer)),
             html`
             <div class="banner banner-info">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
                 you need to <a href="/qz_tray/settings">set up your printer</a> :-P
             </div>
             `
